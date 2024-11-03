4d5213807cfae6c05c5f6af58e3128b8b499e279
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-11-03 18:03:18 +0530

    Hiding PostgreSQL URI

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     """Base Configuration"""
-    SQLALCHEMY_DATABASE_URI = "postgresql://default:NweKl3HzJy9n@ep-shiny-shape-a4x2upf1-pooler.us-east-1.aws.neon.tech:5432/verceldb"
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = 'production'
     SECRET_KEY = os.getenv('SECRET_KEY')
