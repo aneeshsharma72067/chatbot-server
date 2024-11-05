b79923e3b79488f73c93738cdc75df3f759d74d5
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-11-05 14:50:41 +0530

    Fixing verceldb url

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
     """Base Configuration"""
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost/chatbot"
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_ENV = 'development'
+    FLASK_ENV = 'production'
     JWT_SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_TOKEN_LOCATION = ['headers']
     JWT_HEADER_NAME = 'Authorization'
