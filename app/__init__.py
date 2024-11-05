bf110df5de1e67c24455d296b1d1166c88f5168b
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-11-05 15:35:14 +0530

    Resolving CORS Issue

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 jwt = JWTManager(app)
-CORS(app, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 app.register_blueprint(bp)
 
 
